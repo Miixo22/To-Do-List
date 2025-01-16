7929d36797bedb9e3ffe0d19b6d4e03402150376
Miixo22 <mixoelencia@gmail.com>
2025-01-16 11:15:33 +0200

    Added the node.js script, customized the Frontend and added an icon to the headind

---
+++ server.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = 3000
+
+
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`app listening on port http//localhost:${port}`)
+})
+      
